0e7763e48789dafcd81a8e95a3aa37906ae234fe
Dominic Orsi <73092665+DominicOrsi@users.noreply.github.com>
2021-10-16 11:47:37 -0700

    Added parnes loop

--- main.py
+++ main.py
@@ -1,6 +1,16 @@
 import requests
+import pandas as pd
+import json
 
-url = "http://147.222.70.18:8080/ccapi"
+
+url = "http://147.222.70.18:8080/ccapi/ver100/devicestatus/battery"
 
 r = requests.get(url)
+
+battery_df = r.text
+
+for i in battery_df:
+    print(i)
+
+print(battery_df)
 print(r)
