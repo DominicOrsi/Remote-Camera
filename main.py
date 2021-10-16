57a256104a41fa6e82942d288c525344c9774c1f
Dominic Orsi <73092665+DominicOrsi@users.noreply.github.com>
2021-10-16 12:38:24 -0700

    Added a shooting operation

--- main.py
+++ main.py
@@ -2,15 +2,22 @@ import requests
 import pandas as pd
 import json
 
+main_url = "http://147.222.70.18:8080/ccapi/ver100"
 
-url = "http://147.222.70.18:8080/ccapi/ver100/devicestatus/battery"
 
-r = requests.get(url)
+# Battery stuff
+battery_url = main_url + "/devicestatus/battery"
+battery_request = requests.get(battery_url)
+battery_dic = battery_request.text
 
-battery_df = r.text
 
-for i in battery_df:
-    print(i)
+battery_dic = json.loads(battery_dic)
+print("Level:", battery_dic["level"])
 
-print(battery_df)
-print(r)
+# Shooting request
+shooting_url = main_url + "/shooting/control/shutterbutton"
+shooting_response = requests.post(shooting_url, json={"af": True})
+print("\nStatus code:", shooting_response.status_code)
+print("Entire Post Response")
+print(shooting_response.json)
+
